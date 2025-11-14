42c2cc43d4c4a071515dd964d0d825b79eeb52cb
Elmer Novoa Jimenez <elmer.novoa@uniminuto.edu.co>
2025-11-14 10:34:24 -0500

    nuevo proyecto

--- auth/RutasLogisticas/src/main/java/rutaslogisticas/View/UpdateUserRequest.java
+++
@@ -1,9 +0,0 @@
-package rutaslogisticas.View;
-
-public class UpdateUserRequest {
-    public String name;
-    public String email;
-    public String role;
-    public String avatarUrl;
-}
-
--- backend/RutasLogisticas/src/main/java/rutaslogisticas/repository/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package rutaslogisticas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rutaslogisticas.entity.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-    boolean existsByPlate(String plate);
-}
